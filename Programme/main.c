87a6ef889fb13771def4b9c56b4bf02b3267ec7c
DindonDesAlpes <anthony.philippe@isen.yncrea.fr>
2023-04-02 18:38:25 +0200

    Optimisation final

--- Programme/main.c
+++ Programme/main.c
@@ -2,38 +2,52 @@
 
 int main(void){
     while(1){
-		char board[TAILLE][TAILLE];
-		init_board(board);
-
         CleanWindows
 		int choix = menu();
 		switch(choix){
+			case 1:
+				CleanWindows
+				choix = menu_Start1();
+
+				if(choix == 1){
+					CleanWindows
+					choix = menu_Start2();
+
+					char board[TAILLE][TAILLE];
+					init_board(board);
+					if(choix == 1){
+                        Partie * p = malloc(sizeof(Partie));
+                        init_Partie(p, board);
+                        printf("partie IA");
+                    }
+                    else if(choix == 2){
+                        Partie * p = malloc(sizeof(Partie));
+                        init_Partie(p, board);
+                        game_JvJ(p, board);
+                    }
+				}
+
+				else if(choix == 2){
+					Partie * p = import_Partie("partie.txt");
+					if(p == NULL) break;
+
+					char board[TAILLE][TAILLE];
+					init_board(board);
+					charger_Partie(p, board);
+					game_JvJ(p, board);
+					free_Partie(p);
+				}
+				break;
+
+            case 2:
+                menu_Crédits();
+				break;
+
 			default:
-				//free_Partie(p);
 				couleur("31");
 				printf("\t Fermeture \n");
 				couleur("0");
 				return 0;
-				break;
-			case 1:
-				choix = menu_Start1();
-				if(choix == 1){
-					choix = menu_Start2();
-					Partie * p = malloc(sizeof(Partie));
-					init_Partie(p, board);
-					if(choix == 1) printf("partie IA");
-					else if(choix == 2) game_JvJ(p, board);
-				}
-				else if(choix == 2){
-					Partie * p2 = malloc(sizeof(Partie));
-					p2 = import_Partie("partie.txt");
-					charger_Partie(p2, board);
-					game_JvJ(p2, board);
-				}
-				break;
-            case 2:
-                menu_Crédits();
-				break;
 		}
 	}
 
